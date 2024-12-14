56075f1b7cc496f1baac9dda156d1c4b44828fe9
NimerHussain <nimerhussain2424@gmail.com>
2024-12-15 03:16:51 +0500

    Update main.jsx

--- First Class/src/main.jsx
+++ First Class/src/main.jsx
@@ -7,5 +7,10 @@ createRoot(document.getElementById('root')).render(
 
 
   <h1>Nimer Hussain</h1>
+   <h2>Wellcome to this world</h2>
+  <img src="./public/vite.svg" alt=""></img>
+  <h3>ReactJS</h3>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam labore reprehenderit iste sint veniam alias omnis assumenda corporis laudantium repellendus. Saepe pariatur enim, ratione ex voluptatibus possimus aperiam et porro vitae. Maxime nobis illum obcaecati ea laboriosam veniam minus, iste excepturi nostrum eligendi eaque libero sint ad ab quibusdam reiciendis.</p>
+  </div>
 
 )
